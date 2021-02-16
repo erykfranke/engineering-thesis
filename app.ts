d31b93483ceaceccc1798c5bf450469b4da5cadf
Eryk Franke <erykfranke98@gmail.com>
2021-02-16 19:43:27 +0100

    separate chunk preprocessing into folder

--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
 import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import {HeatmapDatabase} from "./database-queries/heatmap-database";
-import {calcChunkSize, indexToPosition, positionToIndex} from "./utils/converter";
-import {ChunkModel} from "./models/chunk.model";
-import {LatLngModel} from "./models/lat-lng.model";
+import {positionToIndex} from "./utils/converter";
 import {GpsModel} from "./models/gps.model";
 import {GlobalDatabase} from "./database-queries/global-database";
+import {chunkPreprocessing} from "./utils/chunk-processing";
 
 const app = express();
 app.use(bodyParser.json());
@@ -41,99 +40,9 @@ app.get('/', async (req: express.Request, res: express.Response) => {
     const disabilitiesIds = req.query.disabilities.toString().split(',');
 
     const chunks = await heatmapDatabase.getChunks(southWestLatIndex, northEastLatIndex, southWestLngIndex, northEastLngIndex, hourRange, dateRange, disabilitiesIds);
-    const mergeSize = calcChunkSize(Number(req.query.zoom));
 
-    if (mergeSize !== 1) {
-        res.send(mergeChunks(chunks, mergeSize));
-    } else {
-        const preparedChunks = [];
-        let maxCount = 0;
-        chunks.forEach((chunk) => {
-            const chunkPosition = indexToPosition(chunk.lat_index, chunk.lng_index);
-            preparedChunks.push({
-                southWest: chunkPosition.start,
-                northEast: chunkPosition.end,
-                count: chunk.count
-            });
-            if (maxCount < Number(chunk.count)) {
-                maxCount = chunk.count;
-            }
-        });
-        res.send({
-            chunks: preparedChunks,
-            maxCount
-        });
-    }
+    res.send(chunkPreprocessing(chunks, Number(req.query.zoom)))
 });
-
-function mergeChunks(chunkBeforeMerge: ChunkModel[], mergeSize) {
-    let maxCount = 0;
-    const mergedChunks = [];
-    while (chunkBeforeMerge.length !== 0) {
-        const chunk = chunkBeforeMerge[0];
-        const newChunkIndexesRange = calcNewMergedChunksIndexes(chunk, mergeSize);
-        const chunkToMerge = [];
-        chunkBeforeMerge = chunkBeforeMerge.filter(
-            item => {
-                if ((item.lat_index >= 0 ? item.lat_index >= newChunkIndexesRange.start.lat : item.lat_index <= newChunkIndexesRange.start.lat)
-                    && (item.lat_index >= 0 ? item.lat_index <= newChunkIndexesRange.end.lat : item.lat_index >= newChunkIndexesRange.end.lat)
-                    && (item.lng_index >= 0 ? item.lng_index >= newChunkIndexesRange.start.lng : item.lng_index <= newChunkIndexesRange.start.lng)
-                    && (item.lng_index >= 0 ? item.lng_index <= newChunkIndexesRange.end.lng : item.lng_index >= newChunkIndexesRange.end.lng)
-                ) {
-                    chunkToMerge.push(item);
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        );
-        let countSum = chunkToMerge.reduce((previousValue, currentValue) => {
-            return previousValue + Number(currentValue.count);
-        }, 0);
-        countSum /= Math.pow(mergeSize, 2);
-        if (maxCount < countSum) {
-            maxCount = countSum;
-        }
-        const chunkBounds = indexToPosition(
-            (newChunkIndexesRange.start.lat >= 0 ? newChunkIndexesRange.start.lat : newChunkIndexesRange.end.lat) / mergeSize,
-            (newChunkIndexesRange.start.lng >= 0 ? newChunkIndexesRange.start.lng : newChunkIndexesRange.end.lng) / mergeSize,
-            mergeSize * 10
-        );
-        mergedChunks.push({
-            southWest: chunkBounds.start,
-            northEast: chunkBounds.end,
-            count: countSum
-        });
-    }
-    return {
-        chunks: mergedChunks,
-        maxCount: maxCount,
-    };
-}
-
-function calcNewMergedChunksIndexes(chunk: ChunkModel, mergeSize: number): { start: LatLngModel, end: LatLngModel } {
-    const latRange = calcIndexRange(chunk.lat_index, mergeSize);
-    const lngRange = calcIndexRange(chunk.lng_index, mergeSize);
-    return {
-        start: {lat: latRange.start, lng: lngRange.start},
-        end: {lat: latRange.end, lng: lngRange.end}
-    };
-}
-
-function calcIndexRange(index, mergeSize): { start: number, end: number } {
-    const isNegative = index < 0;
-    if (isNegative) {
-        index = -(index + 1);
-    }
-    const chunkLatQuarter = Math.abs(Number(index) % mergeSize);
-    let start = Number(index) - chunkLatQuarter;
-    let end = Number(index) + ((mergeSize - 1) - chunkLatQuarter);
-    if (isNegative) {
-        start = -(start + 1);
-        end = -(end + 1);
-    }
-    return {start, end}
-}
 
 function loadGps() {
     setTimeout(async () => {
---
+++ utils/chunk-processing.ts
@@ -0,0 +1,101 @@
+import {ChunkModel} from "../models/chunk.model";
+import {calcChunkSize, indexToPosition} from "./converter";
+import {LatLngModel} from "../models/lat-lng.model";
+
+export function chunkPreprocessing(chunks: ChunkModel[], mapZoom: number) {
+    const mergeSize = calcChunkSize(mapZoom);
+    if (mergeSize === 1) {
+        return singleChunkPreparation(chunks);
+    } else {
+        return mergeChunks(chunks, mergeSize);
+    }
+}
+
+export function singleChunkPreparation(chunks: ChunkModel[]) {
+    const preparedChunks = [];
+    let maxCount = 0;
+    chunks.forEach((chunk) => {
+        const chunkPosition = indexToPosition(chunk.lat_index, chunk.lng_index);
+        preparedChunks.push({
+            southWest: chunkPosition.start,
+            northEast: chunkPosition.end,
+            count: chunk.count
+        });
+        if (maxCount < Number(chunk.count)) {
+            maxCount = chunk.count;
+        }
+    });
+    return {
+        chunks: preparedChunks,
+        maxCount: maxCount,
+    };
+}
+
+export function mergeChunks(chunkBeforeMerge: ChunkModel[], mergeSize: number) {
+    let maxCount = 0;
+    const mergedChunks = [];
+    while (chunkBeforeMerge.length !== 0) {
+        const chunk = chunkBeforeMerge[0];
+        const newChunkIndexesRange = calcNewMergedChunksIndexes(chunk, mergeSize);
+        const chunkToMerge = [];
+        chunkBeforeMerge = chunkBeforeMerge.filter(
+            item => {
+                if ((item.lat_index >= 0 ? item.lat_index >= newChunkIndexesRange.start.lat : item.lat_index <= newChunkIndexesRange.start.lat)
+                    && (item.lat_index >= 0 ? item.lat_index <= newChunkIndexesRange.end.lat : item.lat_index >= newChunkIndexesRange.end.lat)
+                    && (item.lng_index >= 0 ? item.lng_index >= newChunkIndexesRange.start.lng : item.lng_index <= newChunkIndexesRange.start.lng)
+                    && (item.lng_index >= 0 ? item.lng_index <= newChunkIndexesRange.end.lng : item.lng_index >= newChunkIndexesRange.end.lng)
+                ) {
+                    chunkToMerge.push(item);
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        );
+        let countSum = chunkToMerge.reduce((previousValue, currentValue) => {
+            return previousValue + Number(currentValue.count);
+        }, 0);
+        countSum /= Math.pow(mergeSize, 2);
+        if (maxCount < countSum) {
+            maxCount = countSum;
+        }
+        const chunkBounds = indexToPosition(
+            (newChunkIndexesRange.start.lat >= 0 ? newChunkIndexesRange.start.lat : newChunkIndexesRange.end.lat) / mergeSize,
+            (newChunkIndexesRange.start.lng >= 0 ? newChunkIndexesRange.start.lng : newChunkIndexesRange.end.lng) / mergeSize,
+            mergeSize * 10
+        );
+        mergedChunks.push({
+            southWest: chunkBounds.start,
+            northEast: chunkBounds.end,
+            count: countSum
+        });
+    }
+    return {
+        chunks: mergedChunks,
+        maxCount: maxCount,
+    };
+}
+
+export  function calcNewMergedChunksIndexes(chunk: ChunkModel, mergeSize: number): { start: LatLngModel, end: LatLngModel } {
+    const latRange = calcIndexRange(chunk.lat_index, mergeSize);
+    const lngRange = calcIndexRange(chunk.lng_index, mergeSize);
+    return {
+        start: {lat: latRange.start, lng: lngRange.start},
+        end: {lat: latRange.end, lng: lngRange.end}
+    };
+}
+
+export  function calcIndexRange(index, mergeSize): { start: number, end: number } {
+    const isNegative = index < 0;
+    if (isNegative) {
+        index = -(index + 1);
+    }
+    const chunkLatQuarter = Math.abs(Number(index) % mergeSize);
+    let start = Number(index) - chunkLatQuarter;
+    let end = Number(index) + ((mergeSize - 1) - chunkLatQuarter);
+    if (isNegative) {
+        start = -(start + 1);
+        end = -(end + 1);
+    }
+    return {start, end}
+}
